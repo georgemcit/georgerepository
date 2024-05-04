17b54e641f44381ff1b5fabcebd25794fd773a08
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-04 16:18:48 -0400

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,34 @@
+variable "subscription_id"{
+  type=string
+}
+
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "access_tier"{
+  type=string
+  default="Cool"
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+
+variable "environment"{
+ type=string
+ default="staging"
+}
