c0618573ef6f6d1a7aa06368d6ce88549887c2a5
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-05 22:08:49 -0400

    Update george output.tf

--- george output.tf
+++ george output.tf
@@ -1,1 +1,12 @@
-
+output "string_length" {
+  value = length(var.original_string)
+}
+output "uppercase_string" {
+  value = upper(var.original_string)
+}
+output "lowercase_string" {
+  value = lower(var.original_string)
+}
+output "concatenated_string" {
+  value = "${var.additional_string}${var.original_string}"
+}
