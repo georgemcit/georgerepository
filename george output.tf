35233c00597d5f989f067ec95666bd78f1b12ad6
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-05 22:16:36 -0400

    Update george output.tf

--- george output.tf
+++ george output.tf
@@ -13,3 +13,9 @@ output "concatenated_string" {
 output "replaced_string" {
   value = replace(var.original_string, "HELLO", "HI")
 }
+output "substring_example" {
+  value = substr(var.original_string, 6, -1)
+}
+output "split_example" {
+  value = split(" ", var.original_string)
+}
