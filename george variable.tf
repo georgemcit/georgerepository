4ea297af6afd3eefaf3ee11752f74aa3dfb3aaea
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-05 22:51:58 -0400

    Update george variable.tf

--- george variable.tf
+++ george variable.tf
@@ -7,5 +7,16 @@ variable "string_list" {
 variable "additional_string" {
   default = "From Syria "
 }
-
+variable "total_output_george" {
+  type    = list(string)
+  default = [724,238,219,291,555]
+}
+locals {
+  incremented_output_george = [
+    for value in var.total_output_george : tostring((value) + 10)
+  ]
+}
+output "incremented_output_george_show" {
+  value=local.incremented_output_george
+}
 
