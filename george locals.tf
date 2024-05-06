392aed90a5f459ebfb1f27b3f3c390be1bc21413
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-05 22:35:24 -0400

    Update george locals.tf

--- george locals.tf
+++ george locals.tf
@@ -2,5 +2,5 @@ locals{
   total_output = [724,238,219,291,555]
 }
 output "sumotest"{
-    value=sum [for numberoutput in local.total_output: (numberoutput)]
+    value=sum ([for numberoutput in local.total_output: (numberoutput)])
 }
