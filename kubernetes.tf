8f250b0ff7ffcc4b9f0d7b9de3455e092482ad4b
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 21:41:13 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,1 +1,103 @@
+locals{ 
+  cluster_names=["george01","george02","george03","george04","george05"]
+}
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "george_ibrahim_1980_01_05"
+  location = "Canada Central"
+}
+resource "azurerm_kubernetes_cluster" "georgeibrahimcluster1" {
+  name                = "george1"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "george1"
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
 
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster" "georgeibrahimcluster2" {
+  name                = "george2"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "george2"
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster" "georgeibrahimcluster3" {
+  name                = "george3"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "george3"
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster" "georgeibrahimcluster4" {
+  name                = "george4"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "george4"
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster" "georgeibrahimcluster5" {
+  name                = "george5"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "george5"
+
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
