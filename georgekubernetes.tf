635223e2560a7e4aec6837e7f72951d813446d98
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 22:18:59 -0400

    Update georgekubernetes.tf

--- georgekubernetes.tf
+++ georgekubernetes.tf
@@ -64,3 +64,13 @@ variable "tags"{
 output "tags" {
   value = var.tags
 }
+resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
+for_each               = azurerm_kubernetes_cluster.george
+name                   = "${each.key}"
+kubernetes_cluster_id  = each.value.id
+vm_size                = "Standard_DS2_v2"
+node_count             = 1 
+  tags = {
+    Environment = "Production"
+  }
+}
