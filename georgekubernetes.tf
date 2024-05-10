286bf978d6336912d7862cd219cf5f7dcd5415fa
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 22:07:06 -0400

    Update georgekubernetes.tf

--- georgekubernetes.tf
+++ georgekubernetes.tf
@@ -1,4 +1,4 @@
-#This is  Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
+#This is an Azure Montreal College Tutorial for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{
    cluster_list=["montreal","toronto","vancouver","alberta"]
 }
@@ -6,7 +6,28 @@ resource "azurerm_kubernetes_cluster" "georgeibrahimcluster" {
   name                = "georgeibrahimcluster"
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  dns_prefix          = "george"
+  dns_prefix          = "ccrf2301"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+resource "azurerm_kubernetes_cluster" "george" {
+  for_each            = {for cluster in local.cluster_list: cluster=>cluster}
+  name                = "${var.prefix}cluster"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = "ccrf2301"
 
   default_node_pool {
     name       = "default"
