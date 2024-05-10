b3afcab5276c4cb27f966eb088b99576bb454f07
georgemcit <168286516+georgemcit@users.noreply.github.com>
2024-05-09 21:15:40 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,4 @@
+#This is for Storage Account creation--->Storage Container name Creation--->Storage Blob Creation
 locals{
    cluster_list=["montreal","toronto","vancouver","alberta"]
 }
@@ -41,71 +42,4 @@ resource "azurerm_kubernetes_cluster" "george" {
   tags = {
     Environment = "Production"
   }
-}
-output "kube_name"{
-  value=[for cluster in azurerm_kubernetes_cluster.george:cluster.name ]
-}
-variable "identity"{
- type=string
- default="SystemAssigned"
-}
-output "identity" {
-  value = var.identity
-}
-variable "tags"{
- type=string
- default= "Production"
-}
-output "tags" {
-  value = var.tags
-}
-resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
-for_each               = azurerm_kubernetes_cluster.george
-name                   = "${each.key}"
-kubernetes_cluster_id  = each.value.id
-vm_size                = "Standard_DS2_v2"
-node_count             = 1 
-  tags = {
-    Environment = "Production"
-  }
-}
-output "id" {
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.id
-  ]
-}
- 
-output "kube_config" {
-  sensitive = true
-  value = [ 
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config_raw
-  ]
-}
- 
-output "client_key" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_key
-  ]
-}
- 
-output "client_certificate" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.client_certificate
-  ]
-}
- 
-output "cluster_ca_certificate" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.cluster_ca_certificate
-  ]
-}
- 
-output "host" {
-  sensitive = true
-  value = [
-    for cluster in azurerm_kubernetes_cluster.george: cluster.kube_config.0.host
-  ]
 }
